745893f526f3db3707601fce5641e258f217d0d1
Usama Bhakrani <usamabhakrani@gmail.com>
2025-06-24 20:21:57 +0500

    working in sandbox

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,16 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('Car', {
+		year: 2022,
+		color: 'Red',
+		engine: { cylinders: 8 },
+		owner: null,
+		service: undefined
+	});
+
+	const car = await client.hGetAll('Car');
+	console.log(car);
+};
 run();
