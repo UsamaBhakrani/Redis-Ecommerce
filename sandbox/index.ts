35ef9fceb3b503c15d057fdd2e506ce0034645e0
Usama Bhakrani <usamabhakrani@gmail.com>
2025-07-01 20:40:23 +0500

    testing hGetAll

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -8,6 +8,11 @@ const run = async () => {
 	});
 
 	const car = await client.hGetAll('Car');
+
+	if (Object.keys(car).length === 0) {
+		console.log('Car not found');
+		return;
+	}
 	console.log(car);
 };
 run();
