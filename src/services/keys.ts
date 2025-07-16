3c6e4bbdc91c895ee92bebf425310d4cfbfe23f2
Usama Bhakrani <usamabhakrani@gmail.com>
2025-07-16 21:08:01 +0500

    saving and fetching sessions

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,2 +1,3 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
+export const sessionKey = (sessionId: string) => `sessions#${sessionId}`;
